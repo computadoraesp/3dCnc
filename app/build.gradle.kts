61f8f360a2fc629d1ac37f91d2b81715bd9c64aa
computadoraesp <computadoraesp@gmail.com>
2026-08-06 23:29:49 +0200

    fix Failed to resolve: androidx.compose.ui:ui-test-junit4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -85,10 +85,12 @@ dependencies {
     testImplementation(libs.mockk)
     testImplementation(libs.kotlinx.coroutines.test)
 
+    androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
 
+    debugImplementation(platform(libs.androidx.compose.bom))
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
 }
