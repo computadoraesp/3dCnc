9c286a8806e6f9c72c087290937a454c40769a42
computadoraesp <computadoraesp@gmail.com>
2026-08-07 21:45:36 +0200

    Enabled Edge-to-Edge: allow the app to utilize the full screen height.

--- app/src/main/java/com/example/cnc3d/MainActivity.kt
+++ app/src/main/java/com/example/cnc3d/MainActivity.kt
@@ -3,16 +3,18 @@ package com.example.cnc3d
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.example.cnc3d.ui.theme.AppTheme
 import com.example.cnc3d.ui.navigation.AppNavigation
+import com.example.cnc3d.ui.theme.AppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
 
         setContent {
